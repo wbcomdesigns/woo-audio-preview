fa6368b9c363972fc2aa59e20639c65a25e72d87
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2021-02-11 12:33:53 +0530

    PHPCS fixes (includes)

--- includes/class-wc-audio-preview-i18n.php
+++ includes/class-wc-audio-preview-i18n.php
@@ -26,20 +26,20 @@
 class Wc_Audio_Preview_i18n {
 
 
- /**
-  * Load the plugin text domain for translation.
-  *
-  * @since    1.0.0
-  */
- public function load_plugin_textdomain() {
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
 
-  load_plugin_textdomain(
-   'wc-audio-preview',
-   false,
-   dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
-  );
+		load_plugin_textdomain(
+			'wc-audio-preview',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
 
- }
+	}
 
 
 
