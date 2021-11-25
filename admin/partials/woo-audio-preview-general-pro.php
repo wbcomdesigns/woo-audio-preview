1887d60542a7fc895a24c70a604c7919d5d5ba9b
Wbcom Dev <pallavi@wbcomdesigns.com>
2021-11-25 13:31:23 +0530

    Fix Warning Issue

--- admin/partials/woo-audio-preview-general-pro.php
+++ admin/partials/woo-audio-preview-general-pro.php
@@ -24,8 +24,8 @@
 					<td>
 						<label class="wcap-pro-switch">
 							<select id="wcap-pro-btn-display-position" name="wcap_pro_admin_general_option[preview_button_display_position]" disabled>
-								<option value="woocommerce_before_add_to_cart_button" <?php selected( $wcap_pro_display_btn_position, 'woocommerce_before_add_to_cart_button' ); ?>><?php esc_html_e( 'Before Add to cart Button', 'wc-audio-preview' ); ?></option>
-								<option value="woocommerce_after_add_to_cart_button" <?php selected( $wcap_pro_display_btn_position, 'woocommerce_after_add_to_cart_button' ); ?>><?php esc_html_e( 'After Add to cart Button', 'wc-audio-preview' ); ?></option>
+								<option value="woocommerce_before_add_to_cart_button"><?php esc_html_e( 'Before Add to cart Button', 'wc-audio-preview' ); ?></option>
+								<option value="woocommerce_after_add_to_cart_button"><?php esc_html_e( 'After Add to cart Button', 'wc-audio-preview' ); ?></option>
 							</select>
 							<div class="wcap-pro bupr-round"></div>
 						</label>
@@ -42,25 +42,25 @@
 				<tr>
 					<th scope="row"><label for="blogname"><?php esc_html_e( 'Preview Button Text Color', 'wc-audio-preview' ); ?></label></th>
 					<td>
-						<input type="color" name="wcap_pro_admin_general_option[preview_button_text_color]" value="<?php echo esc_attr( $wcap_pro_get_btn_text_color ); ?>" disabled>
+						<input type="color" name="wcap_pro_admin_general_option[preview_button_text_color]" value="" disabled>
 					</td>
 				</tr>
 				<tr>
 					<th scope="row"><label for="blogname"><?php esc_html_e( 'Preview Button Hover Text Color', 'wc-audio-preview' ); ?></label></th>
 					<td>
-						<input type="color" name="wcap_pro_admin_general_option[preview_button_hover_text_color]" value="<?php echo esc_attr( $wcap_pro_get_btn_hover_text_color ); ?>" disabled>
+						<input type="color" name="wcap_pro_admin_general_option[preview_button_hover_text_color]" value="" disabled>
 					</td>
 				</tr>
 				<tr>
 					<th scope="row"><label for="blogname"><?php esc_html_e( 'Preview Button Background Color', 'wc-audio-preview' ); ?></label></th>
 					<td>
-						<input type="color" name="wcap_pro_admin_general_option[preview_button_bg_color]" value="<?php echo esc_attr( $wcap_pro_get_btn_bg_color ); ?>" disabled>
+						<input type="color" name="wcap_pro_admin_general_option[preview_button_bg_color]" value="" disabled>
 					</td>
 				</tr>
 				<tr>
 					<th scope="row"><label for="blogname"><?php esc_html_e( 'Preview Button Hover Background Color', 'wc-audio-preview' ); ?></label></th>
 					<td>
-						<input type="color" name="wcap_pro_admin_general_option[preview_button_hover_bg_color]" value="<?php echo esc_attr( $wcap_pro_get_btn_hover_bg_color ); ?>" disabled>
+						<input type="color" name="wcap_pro_admin_general_option[preview_button_hover_bg_color]" value="" disabled>
 					</td>
 				</tr>
 			</tbody>
